5aa43ef41aa11be926d5ec89a02b73b1a22f5524
ace <1426254402@qq.com>
2024-02-23 12:40:26 +0800

    调色板初步样式+基本换色逻辑

--- app/src/main/java/com/sunnyweather/drawpixel/ui/theme/MainActivity.kt
+++ app/src/main/java/com/sunnyweather/drawpixel/ui/theme/MainActivity.kt
@@ -1,26 +1,54 @@
 package com.sunnyweather.drawpixel.ui.theme
 
 import android.graphics.Color
+import android.graphics.ColorSpace.Rgb
 import android.graphics.drawable.ColorDrawable
 import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.GridLayout
 import android.widget.ImageView
+
 import com.sunnyweather.drawpixel.R
 
 class MainActivity : AppCompatActivity() {
+
+    // 设置一个画笔的颜色
+    var penColor = Color.RED
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 获取调色板的按钮
+        val redBtn = findViewById<Button>(R.id.redBtn)
+        val blueBtn = findViewById<Button>(R.id.blueBtn)
+        val greenBtn = findViewById<Button>(R.id.greenBtn)
+
+        redBtn.setOnClickListener {
+            // 按钮变大
+
+            penColor = Color.RED
+        }
+        blueBtn.setOnClickListener {
+            penColor = Color.BLUE
+
+
+        }
+        greenBtn.setOnClickListener {
+            penColor = Color.GREEN
+
+
+        }
+
+
 
         val gridLayout:GridLayout = findViewById(R.id.grid)
         for (i in 0..48)
         {
             val imageView:ImageView = ImageView(this)
             imageView.setBackgroundColor(Color.RED)
-//            imageView.maxWidth = 10
-//            imageView.maxHeight = 10
+
             imageView.layoutParams = GridLayout.LayoutParams(
                 GridLayout.spec(GridLayout.UNDEFINED,1f),
                 GridLayout.spec(GridLayout.UNDEFINED,1f)
@@ -28,12 +56,7 @@ class MainActivity : AppCompatActivity() {
             imageView.setOnClickListener {
                 val backgroundDrawable = imageView.background
                 if (backgroundDrawable != null && backgroundDrawable is ColorDrawable) {
-                    val color = backgroundDrawable.color
-                    if (color == Color.RED) {
-                        imageView.setBackgroundColor(Color.WHITE)
-                    } else {
-                        imageView.setBackgroundColor(Color.RED)
-                    }
+                    imageView.setBackgroundColor(penColor)
 
                 }
             }
